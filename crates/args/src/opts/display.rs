90a5edd86b886c0d9d9d10abc7620af13bade0b1
Ammar Arif <evergreenkary@gmail.com>
2024-04-06 08:14:28 +0800

    improve display options

--- crates/args/src/opts/display.rs
+++ crates/args/src/opts/display.rs
@@ -1,8 +1,18 @@
 use clap::Args;
-use eyre::{ContextCompat, Result};
+use eyre::{ensure, eyre, Result};
 use serde::Serialize;
+use serde_json::Value;
 
 use crate::fmt::Pretty;
+
+#[derive(Debug, thiserror::Error)]
+#[error("Field not found: {field}.\nAvailable fields: {}", available_fields.join(", "))]
+pub struct FieldNotFoundError {
+    /// The field that was not found.
+    field: String,
+    /// The available fields in the object.
+    available_fields: Vec<String>,
+}
 
 #[derive(Debug, Args)]
 pub struct DisplayOptions {
@@ -23,24 +33,47 @@ impl DisplayOptions {
         #[cfg(windows)]
         let _ = colored_json::enable_ansi_support();
 
-        if self.json {
-            if let Some(ref field) = self.field {
-                self.display_json(
-                    serde_json::to_value(&value)?
-                        .get(field)
-                        .context(format!("no such field exist: {field}"))?,
-                )
-            } else {
-                self.display_json(&serde_json::to_value(&value)?)
-            }
+        if self.json || self.field.is_some() {
+            self.display_json(value)?;
         } else {
             println!("{}", value.prettify());
-            Ok(())
         }
-    }
 
-    fn display_json(&self, value: &serde_json::Value) -> Result<()> {
-        println!("{}", colored_json::to_colored_json_auto(&value)?);
         Ok(())
     }
+
+    fn display_json<T: Serialize>(&self, value: T) -> Result<()> {
+        if let Some(ref field) = self.field {
+            let value = serde_json::to_value(&value)?;
+
+            ensure!(
+                value.is_object(),
+                "Unable to extract field '{field}'. Value is not an object."
+            );
+
+            match value.get(field) {
+                Some(field) => {
+                    println!("{}", colored_json::to_colored_json_auto(field)?);
+                }
+
+                None => {
+                    return Err(eyre!(FieldNotFoundError {
+                        field: field.clone(),
+                        available_fields: keys(&value)
+                    }));
+                }
+            }
+        } else {
+            println!("{}", colored_json::to_colored_json_auto(&value)?);
+        }
+
+        Ok(())
+    }
+}
+
+fn keys(value: &Value) -> Vec<String> {
+    value
+        .as_object()
+        .map(|obj| obj.keys().map(|s| s.to_string()).collect::<Vec<_>>())
+        .unwrap_or_default()
 }
